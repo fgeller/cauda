738ef80232d54feb483a9ecb3c65f4dae7c04545
Felix Geller <fgeller@gmail.com>
2014-06-19 21:49:43 +1200

    Parameterized helper functions.

--- test/cauda/core_test.clj
+++ test/cauda/core_test.clj
@@ -10,16 +10,16 @@
    (swap! user-counter (fn [_] 0))
    (swap! last-pop (fn [_] 0))))
 
-(defn add-test-user []
-  (let [request (body (content-type (request :post "/users") "application/json") "{\"nick\": \"felix\"}")]
+(defn add-test-user [nick]
+  (let [request (body (content-type (request :post "/users") "application/json") (format "{\"nick\": \"%s\"}" nick))]
     (handler request)))
 
 (defn add-test-veto [value]
   (let [request (body (content-type (request :post "/users/1/veto") "application/json") (format "{\"data\": \"%s\"}" value))]
     (handler request)))
 
-(defn queue-test-value [value]
-  (let [request (body (content-type (request :post "/users/1/queue") "application/json") (format "{\"data\":\"%s\"}" value))]
+(defn queue-test-value [user-id value]
+  (let [request (body (content-type (request :post (format "/users/%s/queue" user-id)) "application/json") (format "{\"data\":\"%s\"}" value))]
     (handler request)))
 
 (fact "listing users"
@@ -67,7 +67,7 @@
         (:body response) => "{\"data\":null}"))
 
 (fact "queueing and finding values"
-      (add-test-user)
+      (add-test-user "gerd")
 
       (let [request (body (content-type (request :post "/users/1/queue") "application/json") "{\"data\":\"tnt\"}")
             response (handler request)]
@@ -84,7 +84,7 @@
       (:body response) => "[]"))
 
 (fact "adding a veto means a song is ignored while veto is valid"
-      (add-test-user)
+      (add-test-user "gerd")
       (let [request (body (content-type (request :post "/users/1/veto") "application/json") "{\"data\": \"acme\"}")
             response (handler request)]
         (:status response) => 201)
@@ -93,8 +93,8 @@
         (:status response) => 200
         (:body response) => "[\"acme\"]")
 
-      (queue-test-value "tnt")
-      (queue-test-value "acme")
+      (queue-test-value 1 "tnt")
+      (queue-test-value 1 "acme")
 
       (let [response (handler (request :get "/queue/pop"))]
         (:status response) => 200
@@ -103,14 +103,14 @@
       (against-background (after :facts (cleanup))))
 
 (fact "we drop only veto'd values in front of a selected value"
-      (add-test-user)
+      (add-test-user "gerd")
       (let [request (body (content-type (request :post "/users/1/veto") "application/json") "{\"data\": \"acme\"}")
             response (handler request)]
         (:status response) => 201)
 
-      (queue-test-value "acme")
-      (queue-test-value "tnt")
-      (queue-test-value "acme")
+      (queue-test-value 1 "acme")
+      (queue-test-value 1 "tnt")
+      (queue-test-value 1 "acme")
 
       (let [response (handler (request :get "/users/1/queue"))]
         (:status response) => 200
@@ -127,7 +127,7 @@
       (against-background (after :facts (cleanup))))
 
 (fact "vetos are capped at 5 per user per day"
-      (add-test-user)
+      (add-test-user "gerd")
       (add-test-veto "a")
       (add-test-veto "b")
       (add-test-veto "c")
