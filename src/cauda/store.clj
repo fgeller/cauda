3dae57451b83e7249099593c1fbedb0946340598
Felix Geller <fgeller@gmail.com>
2014-06-22 08:12:02 +1200

    Some more playing during half-time.

--- src/cauda/store.clj
+++ src/cauda/store.clj
@@ -1,5 +1,6 @@
 (ns cauda.store
   (:require [datomic.api :only [q db] :as d]))
+(use 'clojure.pprint)
 
 ;; trying datomic
 (def datomic-uri "datomic:mem://cauda")
@@ -12,5 +13,10 @@
 (def results (d/q '[:find ?c :where [?c :user-counter]] (d/db conn)))
 (count results)
 (def entity (-> conn d/db (d/entity (ffirst results))))
+(d/db conn)
+(d/entity (d/db conn) (ffirst results))
 (keys entity)
 (:user-counter entity)
+(let [db (d/db conn)]
+  (pprint
+   (map #(:user-counter (d/entity db (first %))) results)))
