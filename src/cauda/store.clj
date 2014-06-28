c089e703b4db36a0d1aa4699f2362b79e00e22da
Felix Geller <fgeller@gmail.com>
2014-06-28 14:48:16 +1200

    Rename d/ to peer/

--- src/cauda/store.clj
+++ src/cauda/store.clj
@@ -1,57 +1,57 @@
 (ns cauda.store
-  (:require [datomic.api :only [q db] :as d]))
+  (:require [datomic.api :only [q db] :as peer]))
 
 (def datomic-uri "datomic:mem://cauda")
-(d/create-database datomic-uri)
+(peer/create-database datomic-uri)
 
-(def conn (d/connect datomic-uri))
-(def db (d/db conn))
+(def conn (peer/connect datomic-uri))
+(def db (peer/db conn))
 
 (def schema-tx (read-string (slurp "schema.edn")))
-@(d/transact conn schema-tx)
-(def db (d/db conn))
+@(peer/transact conn schema-tx)
+(def db (peer/db conn))
 
 (defn construct-user [entity]
   {(:user/id entity) {:nick (:user/nick entity)}})
 
 (defn get-all-users-from-db [database]
   (map construct-user
-       (map (fn [[entity-id]] (d/entity database entity-id))
-            (d/q `[:find ?u :where [?u :user/id]] database))))
+       (map (fn [[entity-id]] (peer/entity database entity-id))
+            (peer/q `[:find ?u :where [?u :user/id]] database))))
 
 (get-all-users-from-db db)
 
 (def some-users-tx
   [
-   {:db/id (d/tempid :db.part/user) :user/id 23 :user/nick "hans"}
-   {:db/id (d/tempid :db.part/user) :user/id 24 :user/nick "peter"}
-   {:db/id (d/tempid :db.part/user) :user/id 25 :user/nick "dieter"}
+   {:db/id (peer/tempid :db.part/user) :user/id 23 :user/nick "hans"}
+   {:db/id (peer/tempid :db.part/user) :user/id 24 :user/nick "peter"}
+   {:db/id (peer/tempid :db.part/user) :user/id 25 :user/nick "dieter"}
    ])
-@(d/transact conn some-users-tx)
+@(peer/transact conn some-users-tx)
 
-(def db (d/db conn))
+(def db (peer/db conn))
 (get-all-users-from-db db)
 
 (defn get-user-from-db [database id]
-  (let [entity (d/entity database (ffirst (d/q [:find '?u :where ['?u :user/id id]] database)))]
+  (let [entity (peer/entity database (ffirst (peer/q [:find '?u :where ['?u :user/id id]] database)))]
     (construct-user entity)))
 (get-user-from-db db 23)
 
 (defn update-user-nick [database user-id new-nick]
-  (let [[entity-id] (first (d/q `[:find ?u :where [?u :user/id ~user-id]] database))
+  (let [[entity-id] (first (peer/q `[:find ?u :where [?u :user/id ~user-id]] database))
         update-tx [{:db/id entity-id :user/nick new-nick}]]
-    @(d/transact conn update-tx)))
+    @(peer/transact conn update-tx)))
 
 (update-user-nick db 23 "luigi")
 (get-all-users-from-db db)
 
-(def db (d/db conn))
+(def db (peer/db conn))
 (get-all-users-from-db db)
 
 (def tx-instants (reverse (sort
-                           (d/q '[:find ?when :where [_ :db/txInstant ?when]] db))))
+                           (peer/q '[:find ?when :where [_ :db/txInstant ?when]] db))))
 
 tx-instants
 
-(d/shutdown true)
-(d/delete-database datomic-uri)
+(peer/shutdown true)
+(peer/delete-database datomic-uri)
