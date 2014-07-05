812c7d376b95fc171a71e57aa680ed7b1d1e23b4
Felix Geller <fgeller@gmail.com>
2014-07-05 21:04:43 +1200

    cleanup and refactorings to enable test runner.

--- src/cauda/store.clj
+++ src/cauda/store.clj
@@ -2,56 +2,45 @@
   (:require [datomic.api :only [q db] :as peer]))
 
 (def datomic-uri "datomic:mem://cauda")
-(peer/create-database datomic-uri)
+(defn drop-database [] (peer/shutdown))
+(defn create-database [] (peer/create-database datomic-uri))
+(defn create-database-connection [] (peer/connect datomic-uri))
+(defn read-database [connection] (peer/db connection))
+(defn install-schema [connection]
+  (let [schema-tx (read-string (slurp "schema.edn"))]
+    @(peer/transact connection schema-tx)))
+(defn setup-database []
+  (create-database)
+  (-> (create-database-connection)
+      (install-schema)))
 
-(def conn (peer/connect datomic-uri))
-(def db (peer/db conn))
+(setup-database)
 
-(def schema-tx (read-string (slurp "schema.edn")))
-@(peer/transact conn schema-tx)
-(def db (peer/db conn))
+(def global-connection (create-database-connection))
+(def db (read-database global-connection))
 
-(defn construct-user [entity]
-  {(:user/id entity) {:nick (:user/nick entity)}})
+(defn construct-user [entity] {(:user/id entity) {:nick (:user/nick entity)}})
 
 (defn get-all-users-from-db [database]
   (map construct-user
        (map (fn [[entity-id]] (peer/entity database entity-id))
             (peer/q `[:find ?u :where [?u :user/id]] database))))
 
-(get-all-users-from-db db)
-
-(def some-users-tx
-  [
-   {:db/id (peer/tempid :db.part/user) :user/id 23 :user/nick "hans"}
-   {:db/id (peer/tempid :db.part/user) :user/id 24 :user/nick "peter"}
-   {:db/id (peer/tempid :db.part/user) :user/id 25 :user/nick "dieter"}
-   ])
-@(peer/transact conn some-users-tx)
-
-(def db (peer/db conn))
-(get-all-users-from-db db)
 
 (defn get-user-from-db [database id]
   (let [entity (peer/entity database (ffirst (peer/q '[:find ?u :in $ ?i :where [?u :user/id ?i]] database id)))]
     (construct-user entity)))
 (get-user-from-db db 23)
 
-(defn update-user-nick [database user-id new-nick]
+(defn update-user-nick [connection database user-id new-nick]
   (let [[entity-id] (first (peer/q `[:find ?u :where [?u :user/id ~user-id]] database))
         update-tx [{:db/id entity-id :user/nick new-nick}]]
-    @(peer/transact conn update-tx)))
-
-(update-user-nick db 23 "luigi")
-(def db (peer/db conn))
-(get-all-users-from-db db)
+    @(peer/transact connection update-tx)))
 
 (defn queue-value-for-user [connection database user-id value]
   (let [[user-entity-id] (first (peer/q '[:find ?u :in $ ?i :where [?u :user/id ?i]] database user-id))
         update-tx [{:db/id (peer/tempid :db.part/user) :value/content value :value/queuer user-entity-id :value/queue-time (new java.util.Date)}]]
     @(peer/transact connection update-tx)))
-
-(queue-value-for-user conn db 23 "acme")
 
 (defn queued-entities-for-user [database user-id]
   (map first
@@ -60,22 +49,31 @@
                  [?q :value/queuer ?u]
                  [(missing? $ ?q :value/pop-time)]] database user-id)))
 
-(def db (peer/db conn))
-(queued-entities-for-user db 23)
-
 (defn pop-value-for-user [connection database user-id value]
   (let [users-queue (queued-entities-for-user database user-id)
         value-to-pop (first (sort-by #(:value/queue-time (peer/entity database %)) users-queue))
         update-tx [{:db/id value-to-pop :value/pop-time (new java.util.Date)}]]
     @(peer/transact connection update-tx)))
 
-(pop-value-for-user conn db 23 "acme")
+(defn playing-around []
 
+  (def some-users-tx
+    [
+     {:db/id (peer/tempid :db.part/user) :user/id 23 :user/nick "hans"}
+     {:db/id (peer/tempid :db.part/user) :user/id 24 :user/nick "peter"}
+     {:db/id (peer/tempid :db.part/user) :user/id 25 :user/nick "dieter"}
+     ])
+  @(peer/transact global-connection some-users-tx)
 
-(def tx-instants (reverse (sort
-                           (peer/q '[:find ?when :where [_ :db/txInstant ?when]] db))))
+  (get-all-users-from-db db)
+  (update-user-nick db 23 "luigi")
+  (get-all-users-from-db db)
+  (queue-value-for-user global-connection db 23 "acme")
+  (queued-entities-for-user db 23)
+  (pop-value-for-user global-connection db 23 "acme")
+  (get-all-users-from-db db)
 
-tx-instants
+  (def tx-instants (reverse (sort
+                             (peer/q '[:find ?when :where [_ :db/txInstant ?when]] db))))
 
-(peer/shutdown true)
-(peer/delete-database datomic-uri)
+  tx-instants)
