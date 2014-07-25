58da49cb2364e921390e60bf18df6db83fc0fc8c
Felix Geller <fgeller@gmail.com>
2014-07-25 21:17:51 +1200

    Cleanup.

--- src/cauda/store.clj
+++ src/cauda/store.clj
@@ -22,26 +22,3 @@
 
 (defn global-connection [] (create-database-connection))
 (def db (read-database (global-connection)))
-
-;; (defn playing-around []
-
-;;   (def some-users-tx
-;;     [
-;;      {:db/id (peer/tempid :db.part/user) :user/id 23 :user/nick "hans"}
-;;      {:db/id (peer/tempid :db.part/user) :user/id 24 :user/nick "peter"}
-;;      {:db/id (peer/tempid :db.part/user) :user/id 25 :user/nick "dieter"}
-;;      ])
-;;   @(peer/transact global-connection some-users-tx)
-
-;;   (get-all-users-from-db db)
-;;   (update-user-nick db 23 "luigi")
-;;   (get-all-users-from-db db)
-;;   (queue-value-for-user global-connection db 23 "acme")
-;;   (queued-entities-for-user db 23)
-;;   (pop-value-for-user global-connection db 23 "acme")
-;;   (get-all-users-from-db db)
-
-;;   (def tx-instants (reverse (sort
-;;                              (peer/q '[:find ?when :where [_ :db/txInstant ?when]] db))))
-
-;;   tx-instants)
